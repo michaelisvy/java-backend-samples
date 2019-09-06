25d7faf304354e1ed8d767b4c2cc9b1448300751
Michael <michael.isvy@online-pajak.com>
2019-09-06 15:06:03 +0800

    set h2 as a default profile

--- src/main/java/com/bank/Application.java
+++ src/main/java/com/bank/Application.java
@@ -2,12 +2,33 @@ package com.bank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.SimpleCommandLinePropertySource;
 
 @SpringBootApplication
 public class Application {
 
+	public static final String DEFAULT_PROFILE_NAME = "h2";
+
 	public static void main(String[] args) {
+		SpringApplication application = new SpringApplication(Application.class);
+		assignDefaultProfile(args, application);
+
 		SpringApplication.run(Application.class);
+
+
 	}
+
+	private static void assignDefaultProfile(String[] args, SpringApplication application) {
+		SimpleCommandLinePropertySource source = new SimpleCommandLinePropertySource(args);
+		if (!springProfileHasBeenDefined(source)) {
+			application.setAdditionalProfiles(DEFAULT_PROFILE_NAME);
+		}
+	}
+
+	private static boolean springProfileHasBeenDefined(SimpleCommandLinePropertySource source) {
+		return source.containsProperty("spring.profiles.active") &&
+				System.getenv().containsKey("SPRING_PROFILES_ACTIVE");
+	}
+
 
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,4 @@
+-- Below is executed when spring.jpa.hibernate.ddl-auto is set to `create` or `create-drop`
 insert into customer (id, first_name, last_name, version) values (1,'Jack','Bauer',1);
 insert into account(id, amount, customer_id) values (1, 50, 1);
 insert into account(id, amount, customer_id) values (2, 100, 1);
