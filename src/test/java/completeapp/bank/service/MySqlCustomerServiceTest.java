3e4c48ea02d588ec4a810f0e8c8f1b5c5310f427
Michael <michael.isvy@online-pajak.com>
2019-11-02 14:35:26 +0800

    trying to find a way for Github actions to run embedded h2 tests only

--- src/test/java/completeapp/bank/service/MySqlCustomerServiceTest.java
+++ src/test/java/completeapp/bank/service/MySqlCustomerServiceTest.java
@@ -1,7 +1,10 @@
 package completeapp.bank.service;
 
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 import org.springframework.test.context.ActiveProfiles;
 
+@EnabledOnOs({OS.WINDOWS, OS.MAC})
 @ActiveProfiles("mysql")
 public class MySqlCustomerServiceTest extends CustomerServiceTest {
 }
