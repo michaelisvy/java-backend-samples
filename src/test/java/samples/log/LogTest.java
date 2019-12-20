76e9a9f89eeab3361f83e3882aa24e9064fbf991
Michael <michael.isvy@online-pajak.com>
2019-12-20 14:17:29 +0800

    added SLF4J example using formatting specifics {}

---
+++ src/test/java/samples/log/LogTest.java
@@ -0,0 +1,18 @@
+package samples.log;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class LogTest {
+
+    private static final Logger log = LoggerFactory.getLogger(LogTest.class);
+
+    @Test
+    public void logMessage() {
+        log.info("Date today: {} and Time today: {}", LocalDate.now(), LocalTime.now());
+    }
+}
