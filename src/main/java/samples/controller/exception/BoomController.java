1f7104dca851cc586271b38be64699e3f39f0e2c
Michael <michael.isvy@online-pajak.com>
2020-01-20 15:09:46 +0800

    added more examples about Exceptions with Spring MVC

--- src/main/java/samples/controller/exception/BoomController.java
+++ src/main/java/samples/controller/exception/BoomController.java
@@ -1,7 +1,10 @@
 package samples.controller.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.server.ResponseStatusException;
 import samples.completeapp.bank.model.Customer;
 
 @Controller
@@ -10,5 +13,12 @@ public class BoomController {
     @GetMapping("/boom")
     public Customer boom() {
         throw new BoomException("Boom!!!");
+    }
+
+    @GetMapping("/localBoom")
+    public ResponseEntity<String> localBoom() {
+        String errorMessage = "error came from localBoom";
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, errorMessage,
+                    new RuntimeException(errorMessage));
     }
 }
