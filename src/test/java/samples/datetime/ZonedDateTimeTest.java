5784adad240d9eb9bda2bf043682d7d24ac87177
michaelisvy <michael.isvy@online-pajak.com>
2020-06-03 18:54:16 +0800

    added example on ZoneDateTime

--- src/test/java/samples/datetime/ZonedDateTimeTest.java
+++ src/test/java/samples/datetime/ZonedDateTimeTest.java
@@ -8,11 +8,24 @@ import java.time.ZonedDateTime;
 import static org.assertj.core.api.Assertions.*;
 
 public class ZonedDateTimeTest {
+    public static final String ASIA_JAKARTA_TIMEZONE = "Asia/Jakarta";
+
 
     @Test
     public void shouldValidateTimeEqualityAccrossTimeZones() {
         assertThat(getShanghaiTime()).isEqualTo(getTokyoTime());
+    }
 
+    @Test
+    public void shouldValidateThatDateisInBetween2Dates() {
+        ZonedDateTime jakartaToday = ZonedDateTime.now(ZoneId.of(ASIA_JAKARTA_TIMEZONE));
+        ZonedDateTime jakartaYesterday = jakartaToday.minusDays(1);
+
+        assertThat(jakartaToday.withHour(8).withMinute(0).withSecond(0).withNano(0))
+                .isBetween(
+                        jakartaYesterday.withHour(15).withMinute(0).withSecond(0).withNano(1),
+                        jakartaToday.withHour(8).withMinute(0).withSecond(0).withNano(0)
+        );
     }
 
     private ZonedDateTime getShanghaiTime() {
