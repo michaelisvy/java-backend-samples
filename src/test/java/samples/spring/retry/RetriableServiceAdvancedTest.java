a5ef2e9d1d0deaabab81e771b6437320c7db5b73
michaelisvy <michael.isvy@online-pajak.com>
2020-08-22 17:52:53 +0800

    added more advanced example on Spring retry

---
+++ src/test/java/samples/spring/retry/RetriableServiceAdvancedTest.java
@@ -0,0 +1,22 @@
+package samples.spring.retry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import samples.annotation.OPServiceTest;
+
+import static org.assertj.core.api.Assertions.*;
+
+@OPServiceTest
+public class RetriableServiceAdvancedTest {
+    @Autowired
+    private RetriableServiceAdvanced retriableService;
+
+    @Test
+    public void shouldRetry() {
+        Counter counter = new Counter();
+        this.retriableService.attempt(counter);
+        assertThat(counter.getAttempts()).isEqualTo(5);
+        assertThat(counter.getRecoveries()).isEqualTo(1);
+    }
+
+}
